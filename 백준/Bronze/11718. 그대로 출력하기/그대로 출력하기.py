764206d20c2d85973bdccc9ee102a8cb5012f0ae
sgpirsquare <69654439+sgpirsquare@users.noreply.github.com>
2024-10-16 12:41:21 +0900

    Update 그대로 출력하기.py

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.py
@@ -3,3 +3,16 @@ import sys
 # sys.stdin. 까지는 파일객체랑 같은 역할을 한다. EOF(End of File)
 user_input = sys.stdin.read()
 print(f"{user_input}")
+
+'''
+# 요런 풀이도 있고 와..한줄에 끝내다니
+print(open(0).read())
+
+# 아래 풀이도 꽤 보인다.
+while True:
+  try:
+    a = input()
+    print(a)
+  except:
+    break    
+'''
