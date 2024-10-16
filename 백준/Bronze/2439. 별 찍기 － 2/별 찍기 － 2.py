6c25282007b8277a9e68304e2229ccb699165be4
sgpirsquare <69654439+sgpirsquare@users.noreply.github.com>
2024-10-16 13:13:12 +0900

    Update 별 찍기 － 2.py

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.py
@@ -1,4 +1,13 @@
+# + 와 , 의 차이가 한 끝차이치고는 와 정말
+
 n = int(input())
 
 for i in range(n):
     print(" "*(n-i-1)+"*"*(i+1))
+
+'''
+n = int(input())
+
+for i in range(n):
+    print(" "*(n-i-1), "*"*(i+1))
+'''
