7e28ec7de14692e58fd24fc8e15b7a3248c710e3
sgpirsquare <69654439+sgpirsquare@users.noreply.github.com>
2024-10-25 08:59:27 +0900

    [Bronze V] Title: 크냐?, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4101. 크냐？/크냐？.py
+++ 백준/Bronze/4101. 크냐？/크냐？.py
@@ -1,8 +1,5 @@
 while True:
     a,b = map(int, input().split())
-    if (a,b)==(0,0):
+    if a==0 and b==0:
         break
-    if a>b:
-        print("Yes")
-    else:   
-        print('No')
+    print("Yes" if a>b else "No")
