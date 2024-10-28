a82d12e95eaf854df2d23c7f025e666ac65ce95e
sgpirsquare <69654439+sgpirsquare@users.noreply.github.com>
2024-10-28 11:42:13 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,6 +1,10 @@
-a=int(input())
-b=int(input())
-c=int(input())
-n=list(map(int,str(a*b*c)))
+a = int(input())
+b = int(input())
+c = int(input())
+list_abc = list(str(a * b * c))
 for i in range(10):
-    print(n.count(i))
+    suma = 0
+    for j in range(len(list_abc)):
+        if i == int(list_abc[j]):
+            suma += 1
+    print(suma)
