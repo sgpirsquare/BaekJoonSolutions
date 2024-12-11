d4cd9878ec19395c0c0d12f1673f673067befe59
Jetflash <69654439+sgpirsquare@users.noreply.github.com>
2024-12-11 14:42:27 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+school_names = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy",
+}
+
+acronym = input()
+print(school_names[acronym])
