d65ce69b42ceb77db09cd9a5ddc8f052bb7da632
Jetflash <69654439+sgpirsquare@users.noreply.github.com>
2024-12-26 16:26:24 +0900

    [level 0] Title: 0 떼기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -0,0 +1,9 @@
+def solution(n_str):
+    answer = ''
+    for i in range(len(n_str)): 
+        if n_str[i]!='0':
+            return n_str
+            break
+        else:
+            return str(int(n_str))
+    return answer
